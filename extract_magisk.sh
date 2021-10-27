7e80de958c47e4c6d188f2d88918da1ef931eff4
Kiruya Momochi <65301509+KiruyaMomochi@users.noreply.github.com>
2021-10-27 22:02:56 +0900

    Only need app-debug.apk

--- extract_magisk.sh
+++ extract_magisk.sh
@@ -7,9 +7,13 @@ rm -rf $MagiskExtractFolder
 mkdir -p $MagiskExtractFolder
 
 echo "Extracting Magisk"
-unzip -oj "$MagiskRoot/"app-debug.apk "lib/x86_64/*.so" "lib/x86/libmagisk32.so" -d $MagiskExtractFolder
+unzip -oj "$MagiskRoot/"app-debug.apk "lib/$Architecture/*.so" "lib/x86/libmagisk32.so" -d $MagiskExtractFolder
+unzip -oj "$MagiskRoot/"app-debug.apk "lib/$SystemArchitecture/libmagiskinit.so" -d $MagiskRoot
 
 echo "Renaming files"
+mv $MagiskRoot/libmagiskinit.so $MagiskRoot/magiskpolicy
+chmod 755 $MagiskRoot/magiskpolicy
+
 pushd $MagiskExtractFolder >> /dev/null
 for file in lib*.so; do
   chmod 755 $file
